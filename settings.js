de9e01b0f80b6123c2a884f322d915740e3aaf5b
isithaanusara <isithaanusara01@gmail.com>
2024-09-26 14:02:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.VAJIRA-MD=fYk3XB7Z#TDgis9fGjqlKC55qz5yOwUnvYN8xtp-xQra3Ijh6r_s,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.94723259524,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
